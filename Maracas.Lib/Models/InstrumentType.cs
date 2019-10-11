18f7babf4829d339615c1eee01839347e438d49c
Liro1984 <kenny.zasada@student.howest.be>
2019-10-11 12:05:03 +0200

    Create InstrumentType entity

---
+++ Maracas.Lib/Models/InstrumentType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maracas.Lib.Models
+{
+    class InstrumentType
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
