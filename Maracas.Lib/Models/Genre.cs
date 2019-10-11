8a51759ecd204c8601739c634d14785d54ee0542
Adriana Rodriguez <adriana.rodriguez.gaona@student.howest.be>
2019-10-11 11:31:24 +0200

    Entities made to use like Models

---
+++ Maracas.Lib/Models/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maracas.Lib.Models
+{
+    public class Genre
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
