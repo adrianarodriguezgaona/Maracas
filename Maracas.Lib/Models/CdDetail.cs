45a59a3558286b3ed58880c4bc314ac2ee2d34c7
Adriana Rodriguez <adriana.rodriguez.gaona@student.howest.be>
2019-11-04 20:00:37 +0100

    CdDetails and InstrumentDetails add
    CdDetails has properties ArtiestName , GenreName and ProductPrice

---
+++ Maracas.Lib/Models/CdDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maracas.Lib.Models
+{
+    public class CdDetail
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public long ProductId { get; set; }
+        public decimal ProductPrice { get; set; }
+        public long ArtistId { get; set; }
+        public string ArtistName { get; set; }
+        public long? GenreId { get; set; }
+        public string GenreName { get; set; }
+
+    }
+}
