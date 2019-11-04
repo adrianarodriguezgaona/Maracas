47145c0f8a6ccf777fa2a856c9ca826df1176f36
Adriana Rodriguez <adriana.rodriguez.gaona@student.howest.be>
2019-11-04 20:02:38 +0100

    CdController made with 2 Iactionresults GetCds and GetcdBasic
    Test in Postman oke !

---
+++ MaracasMusic.API/Controllers/CdController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MaracasMusic.API.Data;
+using MaracasMusic.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaracasMusic.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CdController : ControllerBase
+    {
+        CdRepository _cdRepository;
+
+        public CdController(CdRepository cdRepository)
+        {
+            _cdRepository = cdRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetCds()
+        {
+            return Ok(_cdRepository.List());
+        }
+
+        [HttpGet]
+        [Route("Basic")]
+        public IActionResult GetCdsBasic()
+        {
+            return Ok(_cdRepository.ListBasic());
+        }
+    }
+}
