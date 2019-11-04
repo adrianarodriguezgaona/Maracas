0910dbb25dedad50b3ba115b06350ee761b02aee
Adriana Rodriguez <adriana.rodriguez.gaona@student.howest.be>
2019-11-04 19:59:08 +0100

    CdBasicDto and InstrumentBasicDto made with Id en Name property
    if we only need name and Id we use it

---
+++ Maracas.Lib/DTO2/InstrumentBasicDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maracas.Lib.DTO
+{
+    public class InstrumentBasicDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
