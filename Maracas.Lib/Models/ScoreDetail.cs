49091def3894332cfdf0c5c7506e5464494ec5ef
Liro1984 <kenny.zasada@student.howest.be>
2019-11-08 16:22:23 +0100

    add score detail api request to score controller

--- Maracas.Lib/Models/ScoreDetail.cs
+++ Maracas.Lib/Models/ScoreDetail.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Maracas.Lib.Models
 {
-    class ScoreDetail
+   public class ScoreDetail
     {
         public int Id { get; set; }
         public string Name { get; set; }
