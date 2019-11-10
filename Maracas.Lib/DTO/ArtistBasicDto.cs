47c83fdb63941c5c05643606eb0d4019bd1ebaf8
Adriana Rodriguez <adriana.rodriguez.gaona@student.howest.be>
2019-11-10 12:39:29 +0100

    ArtistBasicDto add to have a list of basic properties of artist Nugetpackage Newtonsoft.Json add

---
+++ Maracas.Lib/DTO/ArtistBasicDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maracas.Lib.DTO
+{
+    public class ArtistBasicDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+}
