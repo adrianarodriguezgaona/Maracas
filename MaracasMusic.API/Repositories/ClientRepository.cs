251befa22f0951105ec0e4bbd2fa48c375065259
Liro1984 <kenny.zasada@student.howest.be>
2019-12-26 07:46:34 +0100

    create clientRepository

---
+++ MaracasMusic.API/Repositories/ClientRepository.cs
@@ -0,0 +1,17 @@
+﻿using Maracas.Lib.Models;
+using MaracasMusic.API.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MaracasMusic.API.Repositories
+{
+    public class ClientRepository : RepositoryBase<Client>
+    {
+        public ClientRepository(MaracasContext maracasContext) : base(maracasContext)
+        {
+
+        }
+    }
+}
