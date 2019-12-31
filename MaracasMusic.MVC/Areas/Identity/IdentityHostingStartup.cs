b572396d8343301efaf1daa27e7a68101313666b
Adriana Rodriguez <adriana@codekick.be>
2019-12-31 09:31:37 +0100

    Services folder add with 2 classes to send Email to confirm acount
    User has now first confirms email for inlog

---
+++ MaracasMusic.MVC/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MaracasMusic.MVC.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(MaracasMusic.MVC.Areas.Identity.IdentityHostingStartup))]
+namespace MaracasMusic.MVC.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
