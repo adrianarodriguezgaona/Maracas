f4611c8ca6069c971bbda4c96d5ed4a0b4df6e9c
Adriana Rodriguez <adriana@codekick.be>
2019-12-31 09:34:02 +0100

    Admin action only accessable for authorized user

--- MaracasMusic.MVC/Controllers/HomeController.cs
+++ MaracasMusic.MVC/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MaracasMusic.MVC.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace MaracasMusic.MVC.Controllers
 {
@@ -15,6 +16,7 @@ namespace MaracasMusic.MVC.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult Admin()
         {
             return View();
