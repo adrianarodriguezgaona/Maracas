07f783b40e77e99c1da0d00de6bff34517f00269
Adriana Rodriguez <adriana@codekick.be>
2020-01-03 09:06:57 +0100

    Artists en client controllers inherit controllecrudbase

--- MaracasMusic.API/Controllers/ArtistsController.cs
+++ MaracasMusic.API/Controllers/ArtistsController.cs
@@ -11,56 +11,13 @@ namespace MaracasMusic.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ArtistsController : ControllerBase
+    public class ArtistsController : ControllerCrudBase<Artist, ArtistRepository>
     {
-        readonly ArtistRepository _artistRepository;
-
-        public ArtistsController(ArtistRepository artistRepository)
+      
+        public ArtistsController(ArtistRepository artistRepository) : base(artistRepository)
         {
-            _artistRepository = artistRepository;
+           
         }
-
-        // GET: api/Artists
-        [HttpGet]
-        public async Task<IActionResult> GetArtists()
-        {
-            return Ok(await _artistRepository.ListAll());
-        }
-
-        // GET: api/Artists/Basic
-        [HttpGet]
-        [Route("Basic")]
-        public async Task<IActionResult> GetArtistBasic()
-        {
-            return Ok(await _artistRepository.ListBasic());
-        }
-
-        // GET: api/Artists/2
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetArtist(int id)
-        {
-            return Ok(await _artistRepository.GetById(id));
-        }
-
-        //PUT: api/Artists/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutArtist([FromRoute] int id, [FromBody] Artist artist)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-            if (id != artist.Id)
-            {
-                return BadRequest();
-            }
-
-            Artist updatedArtist = await _artistRepository.Update(artist);
-            if (updatedArtist == null)
-            {
-                return NotFound();
-            }
-            return Ok(updatedArtist);
-        }
+       
     }
 }
--- MaracasMusic.API/Repositories/ClientRepository.cs
+++ MaracasMusic.API/Repositories/ClientRepository.cs
@@ -13,5 +13,6 @@ namespace MaracasMusic.API.Repositories
         {
 
         }
+
     }
 }
