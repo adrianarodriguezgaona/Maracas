e5b8be84ed43f9f8de3f5ea70c5c48c602534281
Adriana Rodriguez <adriana@codekick.be>
2020-01-05 23:01:47 +0100

    Input and button add in panel-products in de admin page to upload a image for a product product.foto has to be equal a name new image!
    images.png , MalumaBoy.png and gallery.png add from the admin view!

--- MaracasMusic.MVC/Controllers/HomeController.cs
+++ MaracasMusic.MVC/Controllers/HomeController.cs
@@ -22,8 +22,6 @@ namespace MaracasMusic.MVC.Controllers
             return View();
         }
 
-       
-
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
